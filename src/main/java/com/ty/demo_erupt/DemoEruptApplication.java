8091366dd9aff5bbd0f1b7b8a2ec46a53aa199f4
kasane <1259117394@qq.com>
2021-11-02 16:41:12 +0800

    test

---
+++ src/main/java/com/ty/demo_erupt/DemoEruptApplication.java
@@ -0,0 +1,13 @@
+package com.ty.demo_erupt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoEruptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoEruptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ty/demo_erupt/DemoEruptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ty.demo_erupt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoEruptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
